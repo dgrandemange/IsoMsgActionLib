ed1f7b16d7a26bf97ec9766c8d8954c9e6a19b25
dgrandemange <damien.grandemange@gmail.com>
2011-04-18 10:25:19 +0200

    update comments in source

--- src/main/org/jpos/jposext/isomsgaction/factory/service/support/ISOMsgActionsConfigDigesterFactoryImpl.java
+++ src/main/org/jpos/jposext/isomsgaction/factory/service/support/ISOMsgActionsConfigDigesterFactoryImpl.java
@@ -1,14 +1,6 @@
 /*
  * Created on 3 avr. 08 by dgrandemange
  *
- * Copyright (c) 2005 Setib
- * All rights reserved.
- *
- * This software is the confidential and proprietary information of
- * Setib ("Confidential Information").  You shall not
- * disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into
- * with Setib.
  */
 package org.jpos.jposext.isomsgaction.factory.service.support;
 
--- src/main/org/jpos/jposext/isomsgaction/model/validation/ValidationRule.java
+++ src/main/org/jpos/jposext/isomsgaction/model/validation/ValidationRule.java
@@ -1,14 +1,6 @@
 /*
  * Created on 20 juil. 07 by dgrandemange
  *
- * Copyright (c) 2005 Setib
- * All rights reserved.
- *
- * This software is the confidential and proprietary information of
- * Setib ("Confidential Information").  You shall not
- * disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license agreement you entered into
- * with Setib.
  */
 package org.jpos.jposext.isomsgaction.model.validation;
 
