0d69649c50002aeec7cadb0f30600e74c699ef68
dgrandemange <damien.grandemange@gmail.com>
2012-07-12 19:41:35 +0200

    convert to maven project

--- wikidoc/src/java/org/jpos/transaction/GroupSelector.java
+++
@@ -1,9 +0,0 @@
-package org.jpos.transaction;
-
-import java.io.Serializable;
-
-public interface GroupSelector extends TransactionParticipant {
-
-	String select(long id, Serializable context);
-
-}
